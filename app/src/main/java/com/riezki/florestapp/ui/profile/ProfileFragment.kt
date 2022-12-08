8ae094dbba8eff24eea96f46eaa529e6a1079381
Riezki Maisyar <riezkymaisyar10@gmail.com>
2022-12-08 13:39:14 +0700

    create login-register

--- app/src/main/java/com/riezki/florestapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/riezki/florestapp/ui/profile/ProfileFragment.kt
@@ -1,15 +1,13 @@
-package com.riezki.florestapp.ui.notifications
+package com.riezki.florestapp.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.riezki.florestapp.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -22,17 +20,15 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
     override fun onDestroyView() {
