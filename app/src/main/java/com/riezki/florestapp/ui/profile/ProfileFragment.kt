f2de724fb1aba31861de62edc81c5938728a4e5f
Riezki Maisyar <riezkymaisyar10@gmail.com>
2022-12-08 22:48:25 +0700

    create ui-home

--- app/src/main/java/com/riezki/florestapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/riezki/florestapp/ui/profile/ProfileFragment.kt
@@ -5,11 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.riezki.florestapp.databinding.FragmentNotificationsBinding
+import com.riezki.florestapp.databinding.FragmentProfileBinding
+
 
 class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -21,7 +22,7 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
 
         return binding.root
     }
