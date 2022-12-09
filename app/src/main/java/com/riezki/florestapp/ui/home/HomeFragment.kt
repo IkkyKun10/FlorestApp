f3725d72ee286302c929fa90860c8d216647e313
Muhammad Irfan Fadhila <irfanfadhila000@gmail.com>
2022-12-10 00:28:10 +0700

    list tips

--- app/src/main/java/com/riezki/florestapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/riezki/florestapp/ui/home/HomeFragment.kt
@@ -7,11 +7,18 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.riezki.florestapp.ListTipsData
+import com.riezki.florestapp.R
+import com.riezki.florestapp.adapter.ListTipsAdapter
 import com.riezki.florestapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private val list : ArrayList<ListTipsData> = arrayListOf()
+    private lateinit var rvListTips: RecyclerView
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,17 +29,46 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+//        val homeViewModel =
+//            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rvListTips = binding.listTipsRv
+        rvListTips.setHasFixedSize(true)
+
+        list.addAll(listTips)
+        showRecycleList()
+    }
+
+    private val listTips: ArrayList<ListTipsData>
+    get() {
+        val judul = resources.getStringArray(R.array.judul)
+        val deskripsi = resources.getStringArray(R.array.deskripsi)
+
+        val listTips = ArrayList<ListTipsData>()
+        for (i in judul.indices) {
+            val list = ListTipsData(judul[i], deskripsi[i])
+            listTips.add(list)
+        }
+        return listTips
+    }
+
+    private fun showRecycleList() {
+        rvListTips.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        val listTipsAdapter = ListTipsAdapter(list)
+        rvListTips.adapter = listTipsAdapter
     }
 
     override fun onDestroyView() {
